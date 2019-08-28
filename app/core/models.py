5d43f16b5867e7d72f41fcb5abbb6d43aa123657
Franco Del Castillo <franco.delcastillo@hotmail.com>
2019-08-28 11:09:14 -0500

    Added custom user model.

---
+++ app/core/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+# we need this to create or user manager class
+# to extend our user model with import
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+
+
+# Manage Class provides helper functions and overwrites to create user
+class UserManager(BaseUserManager):
+    # Creates and saves a new user
+    # **extra_fields adds additional fields to our model
+    def create_user(self, email, password=None, **extra_fields):
+        # raise ValueError if email wasn't provided when function was called
+        if not email:
+            raise ValueError('Users must have an email address')
+        # normalize_email is a helper function from BaseUserManager
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # Password encrypted with set_password
+        user.set_password(password)
+        # supporting multiple databases with _db
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+""" Custom user model extending AbstractBaseUser and PermissionsMixin
+supports using email instead of username """
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    # define the fields for our database model
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # assign user manager to the objects attribute
+    objects = UserManager()
+
+    # customize to use email instead of username
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,41 @@
+# verify user created as expected
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user wit an email is successful"""
+        email = 'test@email.com'
+        password = 'test1234'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        # returns true if password is correct
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@EMAIL.COM'
+        user = get_user_model().objects.create_user(email, 'test1234')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            # everything here should raises a value error
+            get_user_model().objects.create_user(None, 'test1234')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@email.com', 'test1234')
+
+        # assertion here
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
